a8ce7b80f5c64edd46396f612774d0928281472e
tangciwei <tangciwei@baidu.com>
2017-05-31 18:23:05 +0800

    增加普通js格式化

---
+++ src/format-js.js
@@ -0,0 +1,22 @@
+let util = require('./util');
+let exec = require('child_process').exec;
+let fecs = require('fecs');
+let path = require('path');
+
+module.exports = (rootDir = process.cwd()) => {
+
+    let files = util.getAllFiles(rootDir);
+    files = files.filter(item => path.extname(item) === '.js');
+
+    files.forEach(item => {
+        // 文件名
+        let filename = path.basename(item, '.js');
+
+        let options = fecs.getOptions([item, '--replace']);
+
+        fecs.format(options, () => {
+            console.log(filename + '格式化完毕！');
+        });
+
+    });
+};
